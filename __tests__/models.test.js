569f802ddcdd487962151c4fe2cc382e93486408
Adam Lindley <adamlindley98@gmail.com>
2023-03-27 14:01:52 +0100

    complete task 3 get all categories

---
+++ __tests__/models.test.js
@@ -0,0 +1,36 @@
+const request = require("supertest");
+const app = require("../app");
+const seed = require("../db/seeds/seed");
+const testData = require("../db/data/test-data/index");
+const db = require("../db/connection");
+
+beforeEach(() => {
+  return seed(testData);
+});
+
+afterAll(() => {
+  return db.end();
+});
+
+describe("getCategories()", () => {
+  it("should get a reponse of 200: and return all categories", () => {
+    return request(app)
+      .get("/api/categories")
+      .expect(200)
+      .then(({ body }) => {
+        expect(body).toHaveLength(4);
+        body.forEach((category) => {
+          expect(category).toHaveProperty("slug", expect.any(String));
+          expect(category).toHaveProperty("description", expect.any(String));
+        });
+      });
+  });
+  it("should throw a 404 error when given a route that does not exist", () => {
+    return request(app)
+      .get("/api/cat")
+      .expect(404)
+      .then(({ body }) => {
+        expect(body.msg).toBe("Path not found!");
+      });
+  });
+});
